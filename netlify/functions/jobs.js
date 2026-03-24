30a0a60064ca3c6f12366b93c155152e558ad631
Scott Henderson <Scott23Henderson@gmail.com>
2026-03-24 09:22:22 -0700

    Create jobs.js
    API integration

---
+++ netlify/functions/jobs.js
@@ -0,0 +1,24 @@
+export async function handler() {
+  const res = await fetch(
+    `https://api.apify.com/v2/acts/memo23~apify-hiring-cafe-scraper/run-sync-get-dataset-items?token=${process.env.APIFY_TOKEN}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        startUrls: [
+          "https://hiring.cafe/?searchState={\"query\":\"technical project manager\",\"dateFetchedPastNDays\":1}"
+        ],
+        maxItems: 25
+      })
+    }
+  );
+
+  const data = await res.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+}
